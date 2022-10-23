04009dce83898d8c2aaa6ac618ba2563ad200b60
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2022-10-23 20:23:59 +0900

    作成関数を破壊的変更．リトライ回数の制限を追加．

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 use error::ErrorValue;
@@ -8,62 +7,98 @@ use rand::prelude::*;
 mod lexer;
 mod parser;
 mod error;
-use crate::lexer::{lexer};
+use crate::lexer::lexer;
 use crate::parser::*;
 
+
 pub struct Zatlin {
-    operators: RefCell<Vec<Statement>>,
-    text: String,
+    retry_count: u32,
+}
+
+const DEFAULT_RETRY_COUNT: u32 = 100;
+
+impl Default for Zatlin {
+    fn default() -> Self {
+        Self {
+            retry_count: DEFAULT_RETRY_COUNT,
+        }
+    }
 }
 
 impl Zatlin {
-    pub fn new(text: &str) -> Self {
+    pub fn new(retry_count: u32) -> Self {
         Zatlin {
-            operators: RefCell::new(Vec::new()),
-            text: String::from(text),
+            retry_count: if retry_count == 0 {
+                DEFAULT_RETRY_COUNT
+            } else {
+                retry_count
+            },
         }
     }
 
-    pub fn generate(&self) -> Result<String, ErrorValue> {
-        if self.operators.borrow().is_empty() {
-            let tokens = lexer(&self.text);
-            *self.operators.borrow_mut() = parse(&tokens)?;
-        }
-
-        execute(&self.operators)
+    pub fn set_retry(&mut self, count: u32) {
+        self.retry_count = if count < 1 {
+            DEFAULT_RETRY_COUNT
+        } else {
+            count
+        };
     }
 
-    pub fn generate_with(&self, count: i64) -> Result<Vec<String>, ErrorValue> {
-        (0..count).map(|_| self.generate()).collect()
+    pub fn get_retry(&self) {
+        self.retry_count;
+    }
+
+    pub fn generate(&self, text: &str) -> Result<String, ErrorValue> {
+        let tokens = lexer(&text);
+        let statements = parse(&tokens)?;
+        execute(&statements, self.retry_count)
+    }
+
+    pub fn generate_many(&self, text: &str, count: u32) -> Vec<Result<String, ErrorValue>> {
+        let tokens = lexer(&text);
+        let statements = match parse(&tokens) {
+            Ok(statements ) => statements,
+            Err(error) => return vec![Err(error)],
+        };
+
+        let mut result = vec![];
+        let mut i = 0;
+        while i < count {
+            result.push(execute(&statements, self.retry_count));
+            i += 1;
+        }
+
+        result
     }
 }
 
 #[derive(Debug, Clone)]
 struct VariableData {
     pub expression: Rc<Expression>,
+    pub retry_count: u32,
 }
 
 impl VariableData {
-    pub fn new(expression: &Rc<Expression>) -> Self {
+    pub fn new(expression: &Rc<Expression>, retry_count: u32) -> Self {
         Self {
             expression: Rc::clone(&expression),
+            retry_count: retry_count,
         }
     }
 }
 
-fn execute(operators: &RefCell<Vec<Statement>>) -> Result<String, ErrorValue> {
-    let operators = operators.borrow();
+fn execute(operators: &Vec<Statement>, retry_count: u32) -> Result<String, ErrorValue> {
     let mut variables: HashMap<String, VariableData> = HashMap::new();
     let mut random = rand::thread_rng();
 
     for operator in operators.iter() {
         match operator {
             Statement::Define(DefineStruct { name: key, expr }) => {
-                let data = VariableData::new(&expr);
+                let data = VariableData::new(&expr, retry_count);
                 variables.insert(key.to_string(), data);
             },
             Statement::Generate(expr) => {
-                return execute_expression(&VariableData::new(&expr), &variables, &mut random)
+                return execute_expression(&VariableData::new(&expr, retry_count), &variables, &mut random)
             },
         };
     }
@@ -74,7 +109,12 @@ fn execute(operators: &RefCell<Vec<Statement>>) -> Result<String, ErrorValue> {
 fn execute_expression(data: &VariableData, variables: &HashMap<String, VariableData>, random: &mut ThreadRng) -> Result<String, ErrorValue> {
     let max: usize = data.expression.patterns.iter().map(|x| x.count).sum();
 
-    let result = loop {
+    let mut count: u32 = 0;
+    loop {
+        if count >= data.retry_count {
+            break Err(ErrorValue::OverRetryCount);
+        }
+
         let mut rng = random.clone();
         let value = rng.gen_range(0..max);
     
@@ -96,10 +136,11 @@ fn execute_expression(data: &VariableData, variables: &HashMap<String, VariableD
         let result = execute_pattern(&pattern, &variables, &mut rng)?;
 
         if !contains_excludes(&data.expression.excludes, &result) {
-            break result;
+            break Ok(result);
         }
-    };
-    Ok(result)
+
+        count += 1;
+    }
 }
 
 fn contains_excludes(excludes: &Vec<Pattern>, result: &str) -> bool {
@@ -144,9 +185,9 @@ fn execute_value(value: &Value, variables: &HashMap<String, VariableData>, rando
 mod generate_test {
     use crate::error::ErrorValue;
 
-    fn execute(s: &str) -> Result<Vec<String>, ErrorValue> {
-        let zatlin = crate::Zatlin::new(s);
-        zatlin.generate_with(32)
+    fn execute(s: &str) -> Vec<Result<String, ErrorValue>> {
+        let zatlin = crate::Zatlin::default();
+        zatlin.generate_many(&s, 32)
     }
 
     #[test]
@@ -170,15 +211,17 @@ mod generate_test {
         % Cs Vx Ce | Cs Vx Ce Cs Vx Ce - ^ "y" | ^ "ý" | ^ "ỳ" | ^ "ÿ" | ^ "wu" | ^ "wú" | ^ "wù" | ^ "wü" | ^ "hy" | ^ "hý" | ^ "hỳ" | ^ "hÿ" | ^ "qy" | ^ "qý" | ^ "qỳ" | ^ "qÿ" | ^ "ry" | ^ "rý" | ^ "rỳ" | ^ "rÿ" | ^ "ny" | ^ "ný" | ^ "nỳ" | ^ "nÿ" | ^ "my" | ^ "mý" | ^ "mỳ" | ^ "mÿ";
         "#);
         
-        match &result {
-            Ok(value) => {
-                println!("{}", value.join(" "));
-            },
-            Err(message) => {
-                println!("{}", message);
-            },
+        for item in result.iter() {
+            match item {
+                Ok(value) => {
+                    print!("{} ", value);
+                },
+                Err(message) => {
+                    print!("({}) ", message);
+                },
+            }
         }
-        assert!(result.is_ok());
+        assert!(result.iter().all(|x| x.is_ok()));
     }
 
     #[test]
@@ -202,37 +245,45 @@ mod generate_test {
         % Cs Vx Ce | Cs Vx Ce Cs Vx Ce - ^ "y" | ^ "ý" | ^ "ỳ" | ^ "ÿ" | ^ "wu" | ^ "wú" | ^ "wù" | ^ "wü" | ^ "hy" | ^ "hý" | ^ "hỳ" | ^ "hÿ" | ^ "qy" | ^ "qý" | ^ "qỳ" | ^ "qÿ" | ^ "ry" | ^ "rý" | ^ "rỳ" | ^ "rÿ" | ^ "ny" | ^ "ný" | ^ "nỳ" | ^ "nÿ" | ^ "my" | ^ "mý" | ^ "mỳ" | ^ "mÿ";
         "#);
         
-        match &result {
-            Ok(value) => {
-                println!("{}", value.join(" "));
-            },
-            Err(message) => {
-                println!("{}", message);
-            },
+        for item in result.iter() {
+            match item {
+                Ok(value) => {
+                    print!("{} ", value);
+                },
+                Err(message) => {
+                    print!("({}) ", message);
+                },
+            }
         }
-        assert!(result.is_ok());
+        assert!(result.iter().all(|x| x.is_ok()));
     }
-
 
     #[test]
     fn undefined_variable() {
         let result = execute(r#"
         C = "p" | "f" | "t" | "s" | "k" | "h";
         V = "a" | "i" | "u"
+        Y = C V
 
         # 'X' of variable is not defined.
-        % C V | C V C | X | V C | V C V;
+        % X;
         "#);
 
-        match &result {
-            Ok(value) => {
-                println!("{}", value.join(" "));
-            },
-            Err(message) => {
-                println!("{}", message);
-            },
-        }
-        assert!(result.is_err());
-        assert_eq!(result.unwrap_err(), ErrorValue::NotFoundVariable(String::from("X")))
+        assert!(result.iter().all(|x| x.is_err()));
+        assert!(result.iter().all(|x| if let Err(ErrorValue::NotFoundVariable(message)) = x { message == "X" } else { false }))
+    }
+
+    #[test]
+    fn over_retry_count() {
+        let result = execute(r#"
+        C = "p" | "f" | "t" | "s" | "k" | "h";
+        V = "a" | "i" | "u"
+
+        # Retry count is over limit.
+        % C V - "a" ^ | "i" ^ | "u" ^;
+        "#);
+
+        assert!(result.iter().all(|x| x.is_err()));
+        assert!(result.iter().all(|x| if let Err(ErrorValue::OverRetryCount) = x { true } else { false }))
     }
 }

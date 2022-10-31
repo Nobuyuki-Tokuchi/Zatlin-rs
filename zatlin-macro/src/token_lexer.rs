d383c5347b4a853c9fe69309433cee6eb5195146
Nobuyuki-Tokuchi <svalk.and.belnia.65535@gmail.com>
2022-11-01 00:13:52 +0900

    unofficialの対応中にvec![&str]を作成すればいいことに気付いたため変更．

--- zatlin-macro/src/token_lexer.rs
+++ zatlin-macro/src/token_lexer.rs
@@ -12,51 +12,44 @@ use quote::quote;
 
 pub(crate) fn convert_zatlin(input: ParseStream) -> Result<TokenStream> {
     let mut tokens: Vec<TokenStream> = vec![];
-    let token_list_name = Ident::new("__t_i",Span::call_site());
 
     while !input.is_empty() {
         let head = input.lookahead1();
 
         if head.peek(Token![%]) {
             input.parse::<Token![%]>()?;
-            tokens.push(quote!{
-                #token_list_name.push("%");
-            });
-            tokens.push(convert_expression(&token_list_name, input)?);
+            tokens.push(quote!{ "%" });
+            tokens.push(convert_expression(input)?);
             input.parse::<Token![;]>()?;
-            tokens.push(quote!{
-                #token_list_name.push(";");
-            });
+            tokens.push(quote!{ ";" });
         } else if head.peek(Ident) {
             let variable_name = input.parse::<Ident>()?.to_string();
             input.parse::<Token![=]>()?;
             tokens.push(quote! {
-                #token_list_name.append(&mut vec![
-                    #variable_name,
-                    "="
-                ]);
+                #variable_name,
+                "="
             });
-            tokens.push(convert_expression(&token_list_name, input)?);
+            tokens.push(convert_expression(input)?);
             input.parse::<Token![;]>()?;
-            tokens.push(quote!{
-                #token_list_name.push(";");
-            });
+            tokens.push(quote!{ ";" });
         }
     }
 
+    let token_list_name = Ident::new("__t_i",Span::call_site());
     Ok(quote!{
         {
-            let mut #token_list_name: std::vec::Vec<&str> = vec![];
-            #(#tokens)*
+            let mut #token_list_name: std::vec::Vec<&str> = vec![
+                #(#tokens),*
+            ];
             zatlin_internal::ZatlinData::try_from(#token_list_name)
         }
     })
 }
 
-fn convert_expression(key: &Ident, input: ParseStream) -> Result<TokenStream> {
+fn convert_expression(input: ParseStream) -> Result<TokenStream> {
     let mut patterns = Vec::new();
     let mut has_extract = false;
-    patterns.push(convert_pattern(key, input, false)?);
+    patterns.push(convert_pattern(input, false)?);
 
     while !input.is_empty() {
         let head = input.lookahead1();
@@ -69,31 +62,31 @@ fn convert_expression(key: &Ident, input: ParseStream) -> Result<TokenStream> {
             break;
         } else if head.peek(Token![|]) {
             input.parse::<Token![|]>()?;
-            patterns.push(quote!{ #key.push("|"); });
-            patterns.push(convert_pattern(key, input, false)?);
+            patterns.push(quote!{ "|" });
+            patterns.push(convert_pattern(input, false)?);
         } else {
             return Err(Error::new(input.span(), "invalid token"));
         }
     }
 
     if has_extract {
-        patterns.push(quote!{ #key.push("-"); });
+        patterns.push(quote!{ "-" });
         if input.is_empty() {
             return Err(Error::new(input.span(), "end of statement in Exclude Pattern"));
         }
 
         if let Ok(_) = input.parse::<Token![^]>() {
-            patterns.push(quote!{ #key.push("^"); });
+            patterns.push(quote!{ "^" });
         }
 
-        let pattern = match convert_pattern(key, input, true) {
+        let pattern = match convert_pattern(input, true) {
             Ok(t) => t,
             Err(_) => return Err(Error::new(input.span(), "invalid Exclude Pattern")),
         };
         patterns.push(pattern);
 
         if let Ok(_) = input.parse::<Token![^]>() {
-            patterns.push(quote!{ #key.push("^"); });
+            patterns.push(quote!{ "^" });
         }
 
         while !input.is_empty() {
@@ -105,14 +98,14 @@ fn convert_expression(key: &Ident, input: ParseStream) -> Result<TokenStream> {
                 input.parse::<Token![|]>()?;
 
                 if let Ok(_) = input.parse::<Token![^]>() {
-                    patterns.push(quote!{ #key.push("^"); });
+                    patterns.push(quote!{ "^" });
                 }
 
-                patterns.push(quote!{ #key.push("|"); });
-                patterns.push(convert_pattern(key, input, true)?);
+                patterns.push(quote!{ "|" });
+                patterns.push(convert_pattern(input, true)?);
 
                 if let Ok(_) = input.parse::<Token![^]>() {
-                    patterns.push(quote!{ #key.push("^"); });
+                    patterns.push(quote!{ "^" });
                 }
             } else {
                 return Err(Error::new(input.span(), "invalid token"));
@@ -122,11 +115,11 @@ fn convert_expression(key: &Ident, input: ParseStream) -> Result<TokenStream> {
     }
 
     Ok(quote!{
-        #(#patterns)*
+        #(#patterns),*
     })
 }
 
-fn convert_pattern(key: &Ident, input: ParseStream, is_exclude: bool) -> Result<TokenStream> {
+fn convert_pattern(input: ParseStream, is_exclude: bool) -> Result<TokenStream> {
     let mut values = Vec::new();
 
     while !input.is_empty() {
@@ -155,9 +148,7 @@ fn convert_pattern(key: &Ident, input: ParseStream, is_exclude: bool) -> Result<
         Err(Error::new(input.span(), "no pattern"))
     } else {
         Ok(quote!{
-            #key.append(&mut vec![
-                #(#values),*
-            ]);
+            #(#values),*
         })
     }
 }
